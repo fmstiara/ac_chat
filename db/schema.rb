1841177c3991a11599f12cd6e2ec06053d693827
TairaOuchi <taira.o19@gmail.com>
2019-11-04 15:41:22 +0900

    PR test

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_12_041004) do
+ActiveRecord::Schema.define(version: 2019_10_16_051631) do
+
+  create_table "addresses", force: :cascade do |t|
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "entries", force: :cascade do |t|
     t.integer "user_id"
